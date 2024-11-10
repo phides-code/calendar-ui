2dab0b30c7fddcde021d33b8df4446c09184a9e9
Philippe DeSousa <phil.desousa@gmail.com>
2024-11-10 10:16:44 -0500

    setup basic modal

--- src/MainComponent.tsx
+++ src/MainComponent.tsx
@@ -1,32 +1,42 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import { Value } from 'react-calendar/src/shared/types.js';
+import DateModal from './DateModal';
+import { datesWithEvents } from './data';
 
 const MainComponent = () => {
-    // example highlighted date
-    const datesWithEvents = [new Date('2024-11-29T03:24:00')];
-
     const [selectedDate, setSelectedDate] = useState<Date | null>();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
 
     const handleOnChange = (date: Value) => {
         setSelectedDate(date as Date);
+        openModal();
     };
 
-    console.log('selectedDate: ', selectedDate?.toString());
-
     return (
-        <Calendar
-            tileClassName={({ date, view }) =>
-                view === 'month' &&
-                datesWithEvents.some(
-                    (d) => d.toDateString() === date.toDateString()
-                )
-                    ? 'event-day' // Apply 'event-day' class to event dates
-                    : null
-            }
-            onChange={handleOnChange}
-            value={selectedDate}
-        />
+        <>
+            <Calendar
+                tileClassName={({ date, view }) =>
+                    view === 'month' &&
+                    datesWithEvents.some(
+                        (d) => d.toDateString() === date.toDateString()
+                    )
+                        ? 'event-day' // Apply 'event-day' class to event dates
+                        : null
+                }
+                onChange={handleOnChange}
+                value={selectedDate}
+            />
+            {isModalOpen && (
+                <DateModal
+                    closeModal={closeModal}
+                    selectedDate={selectedDate as Date}
+                />
+            )}
+        </>
     );
 };
 
---
+++ src/data.ts
@@ -0,0 +1,2 @@
+// example highlighted date
+export const datesWithEvents = [new Date('2024-11-29T03:24:00')];
